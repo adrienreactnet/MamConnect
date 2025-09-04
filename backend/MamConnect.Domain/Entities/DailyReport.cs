496f5e4ded5fff3adc7c8343dc48733df9f205e8
AdrienKaliva <adrien.fouga@kaliva.fr>
2025-09-04 16:52:21 +0200

    Add DailyReport entity and migration

---
+++ backend/MamConnect.Domain/Entities/DailyReport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MamConnect.Domain.Entities
+{
+    public class DailyReport
+    {
+        public int Id { get; set; }
+        public int ChildId { get; set; }
+        public string Content { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+    }
+}
--- backend/MamConnect.Infrastructure/Data/AppDbContext.cs
+++ backend/MamConnect.Infrastructure/Data/AppDbContext.cs
@@ -8,4 +8,5 @@ namespace MamConnect.Infrastructure.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<Child> Children => Set<Child>();
+    public DbSet<DailyReport> DailyReports => Set<DailyReport>();
 }
